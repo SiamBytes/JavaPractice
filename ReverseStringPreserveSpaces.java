397ab110afdf6a11f2e868f631920bdd269d6964
SiamBytes <mr.mercurysiam@gmail.com>
2025-10-16 20:27:04 -0400

    feat: added Java program to reserve string while preservingspaces

---
+++ ReverseStringPreserveSpaces.java
@@ -0,0 +1,26 @@
+public class ReverseStringPreserveSpaces {
+    public static void main(String[] args) {
+        String input = "I am QA Tester";
+        char[] str = input.toCharArray();
+        char[] result = new char[str.length];
+
+        // Mark spaces in the result
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] == ' ') {
+                result[i] = ' ';
+            }
+        }
+
+        int j = str.length - 1;
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] != ' ') {
+                if (result[j] == ' ') j--;
+                result[j] = str[i];
+                j--;
+            }
+        }
+
+        System.out.println("Input:  " + input);
+        System.out.println("Output: " + String.valueOf(result));
+    }
+}
